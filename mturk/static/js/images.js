4bd67464bbe6ea8b0aca9c0fee06a620df47331e
Qiao Yan <alice.yan@hotmail.com>
2016-07-13 11:37:46 -0700

    database test

--- mturk/static/js/images.js
+++ mturk/static/js/images.js
@@ -10080,7 +10080,7 @@ var len = urls.length;
 // document.getElementById("images").innerHTML = out;
 
 // var ran = Math.floor(Math.random() * len);
-var ran = parseInt(document.getElementById("image_index").value);;
+var ran = parseInt(document.getElementById("image_index").value);
 if (isNaN(ran)) {
     ran = 0;
 }
