23d8ff8e9081edda49168861ac80890ed4907133
Qiao Yan <alice.yan@hotmail.com>
2016-07-13 12:15:57 -0700

    database test

--- mturk/views.py
+++ mturk/views.py
@@ -11,38 +11,41 @@ AMAZON_HOST = "https://workersandbox.mturk.com/mturk/externalSubmit"
 
 @csrf_exempt
 def index(request):
-    if request.GET.get("image_index"):
-        Task.objects.create(workerId = request.GET.get("workerId", ""), 
-            assignmentId = request.GET.get("assignmentId", "")) 
-            # result = request.GET.get("user-input", "")
-            # image_index = int(request.GET.get("image_index", "")))
-        db_rows = Task.objects.filter(workerId = request.GET.get("workerId", ""))
-        render_data = {
-            "worker_id": request.GET.get("workerId", ""),
-            "assignment_id": request.GET.get("assignmentId", ""),
-            "amazon_host": AMAZON_HOST,
-            "hit_id": request.GET.get("hitId", ""),
-            "image_index": str(len(db_rows))
-        }
-    else: 
-        if request.GET.get("assignmentId") == "ASSIGNMENT_ID_NOT_AVAILABLE":
-            # worker hasn't accepted the HIT (task) yet
-            pass
-        else:
-            # worked accepted the task
-            pass
+    # if request.GET.get("image_index"):
+    #     Task.objects.create(workerId = request.GET.get("workerId", ""), 
+    #         assignmentId = request.GET.get("assignmentId", "")) 
+    #         # result = request.GET.get("user-input", "")
+    #         # image_index = int(request.GET.get("image_index", "")))
+    #     db_rows = Task.objects.filter(workerId = request.GET.get("workerId", ""))
+    #     render_data = {
+    #         "worker_id": request.GET.get("workerId", ""),
+    #         "assignment_id": request.GET.get("assignmentId", ""),
+    #         "amazon_host": AMAZON_HOST,
+    #         "hit_id": request.GET.get("hitId", ""),
+    #         "image_index": str(len(db_rows))
+    #     }
+    # else: 
+    if request.GET.get("assignmentId") == "ASSIGNMENT_ID_NOT_AVAILABLE":
+        # worker hasn't accepted the HIT (task) yet
+        pass
+    else:
+        # worked accepted the task
+        pass
 
-        worker_id = request.GET.get("workerId", "")
-        # if worker_id in get_worker_ids_past_tasks():
-        #     # you might want to guard against this case somehow
-        #     pass
+    worker_id = request.GET.get("workerId", "")
+    Task.objects.create(workerId = request.GET.get("workerId", ""), 
+        assignmentId = request.GET.get("assignmentId", ""))
+    # if worker_id in get_worker_ids_past_tasks():
+    #     # you might want to guard against this case somehow
+    #     pass
 
-        render_data = {
-            "worker_id": request.GET.get("workerId", ""),
-            "assignment_id": request.GET.get("assignmentId", ""),
-            "amazon_host": AMAZON_HOST,
-            "hit_id": request.GET.get("hitId", ""),
-        }
+    render_data = {
+        "worker_id": request.GET.get("workerId", ""),
+        "assignment_id": request.GET.get("assignmentId", ""),
+        "amazon_host": AMAZON_HOST,
+        "hit_id": request.GET.get("hitId", ""),
+        "image_index": str(len(db_rows))
+    }
 
     response = render_to_response("index.html", render_data)
     # without this header, your iFrame will not render in Amazon
