4b15341f871957608244f4083a42febdaa3da792
Qiao Yan <alice.yan@hotmail.com>
2016-07-20 13:55:38 -0700

    tag test

--- mturk/models.py
+++ mturk/models.py
@@ -5,8 +5,8 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
-	occasion = models.IntegerField()
-	season = models.IntegerField()
-	style = models.IntegerField()
-	ethnicity = models.IntegerField()
-	body_shape = models.IntegerField()
+	occasion = models.IntegerField(default=0)
+	season = models.IntegerField(default=0)
+	style = models.IntegerField(default=0)
+	ethnicity = models.IntegerField(default=0)
+	body_shape = models.IntegerField(default=0)
