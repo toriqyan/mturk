659cc10881901139e976f74a24e24de528e9745a
Qiao Yan <alice.yan@hotmail.com>
2016-08-02 17:06:25 -0700

    last page check

--- mturk/static/js/main.js
+++ mturk/static/js/main.js
@@ -143,7 +143,7 @@ function nextstep() {
 					result+=i+" "+cur_tag+"\n";
 				});
 				if (result == "") {
-					alert("You have to select a tag for all images";
+					alert("You have to select a tag for all images");
 					return;
 				}
 				$('#images'+i+' input[type="checkbox"]:checked').each(function() {
@@ -159,7 +159,7 @@ function nextstep() {
 					result+="spam_c "+(spam_img_i)+' '+cur_tag+"\n";
 				});
 				if (result == "") {
-					alert("You have to select a tag for all images";
+					alert("You have to select a tag for all images");
 					return;
 				}
 				$('#spam_checker input[type="checkbox"]:checked').each(function() {
@@ -183,7 +183,7 @@ function nextstep() {
 					result+=i+" "+cur_tag+"\n";
 				});
 				if (result == "") {
-					alert("You have to select a tag for all images";
+					alert("You have to select a tag for all images");
 					return;
 				}
 				$('#images'+i+' input[type="checkbox"]:checked').each(function() {
@@ -199,7 +199,7 @@ function nextstep() {
 					result+="spam_c "+(spam_img_i)+' '+cur_tag+"\n";
 				});
 				if (result == "") {
-					alert("You have to select a tag for all images";
+					alert("You have to select a tag for all images");
 					return;
 				}
 				$('#spam_checker input[type="checkbox"]:checked').each(function() {
