bd63d52b2e27f4b0746143fdd95c325ffb538ca2
Qiao Yan <alice.yan@hotmail.com>
2016-08-24 14:05:11 -0700

    test search tool get

--- mturk/models.py
+++ mturk/models.py
@@ -4,9 +4,3 @@ from django.db import models
 
 # Create your models here.
 
-class Task(models.Model):
-	occasion = models.IntegerField(default=0)
-	season = models.IntegerField(default=0)
-	style = models.IntegerField(default=0)
-	ethnicity = models.IntegerField(default=0)
-	body_shape = models.IntegerField(default=0)
