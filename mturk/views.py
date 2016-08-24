211ff6fdb1aa25d12b0b5e88c68e24270e6416e8
Qiao Yan <alice.yan@hotmail.com>
2016-08-24 14:17:47 -0700

    test search tool get

--- mturk/views.py
+++ mturk/views.py
@@ -8,7 +8,7 @@ import json
 
 @csrf_exempt
 def index(request):
-    print(request.GET)
+    print(dict(request.GET))
     if (request.GET.get("response", "") != ""):
         return display(request)
     print("index")
