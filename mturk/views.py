cf9db3d4a0923ab00f9a5206e81748117131752d
Qiao Yan <alice.yan@hotmail.com>
2016-08-24 14:06:52 -0700

    test search tool get

--- mturk/views.py
+++ mturk/views.py
@@ -7,9 +7,11 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 
 @csrf_exempt
+print(request)
 def index(request):
     if (request.GET.get("response", "") != ""):
         return display(request)
+    print("index")
     render_data = {}
     response = render_to_response("index.html", render_data)
     # without this header, your iFrame will not render in Amazon
@@ -18,6 +20,7 @@ def index(request):
 
 @csrf_exempt
 def display(request):
+    print("display")
     array = json.loads(request.GET.get("response", ""))
     render_data = {
         "images": array,
